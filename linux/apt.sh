dd1d870a8ef99c1881a4b4ed0d32317a91f1d384
Aidan Stansfield <aidan.stansfield@gmail.com>
2020-06-11 14:43:35 +1000

    added git

--- linux/apt.sh
+++ linux/apt.sh
@@ -1,6 +1,7 @@
 # install necessary packages with apt
 sudo apt install neovim -y
 sudo apt install fzf -y
+sudo apt install git -y
 
 # install vim-plug for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
